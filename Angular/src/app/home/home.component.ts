0b575af23c915e63dba0b520518ea59e445c4925
saravana-ganesh <saravanaganesh@protonmail.com>
2021-06-30 21:42:05 +0530

    Latest Version

--- Angular/src/app/home/home.component.ts
+++ Angular/src/app/home/home.component.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
 import jspdf from 'jspdf';
 import html2canvas from 'html2canvas';
+
+import { MiddlwareService } from '../services/middlware.service';
+import { EnumValues } from '../classes/EnumValues';
+
 
 declare var jsPDF: any;
 
@@ -10,9 +16,52 @@ declare var jsPDF: any;
   styleUrls: ['./home.component.css']
 })   
 export class HomeComponent implements OnInit {
- 
-  constructor() { }
+  public chartName="Bar Chart";
+  isValidUser = false;
+  //public barChartLabels = ['2006','2007','2008','2009','2010','2011','2012'];
+  public barChartLabels:any = [];
+  public barChartValues:any = [];
+  //public barChartType = 'bar';
+  public barChartType : EnumValues = EnumValues.bar;
+  public barChartLegend = true;
+
+  constructor(
+    private router:Router,
+    private middlwareService:MiddlwareService
+  ) { }
   ngOnInit() {
+    let userDetails = {
+      email:localStorage.getItem('email'),
+      key:localStorage.getItem('tokenID')
+    };
+    this.middlwareService.home(JSON.stringify(userDetails)).subscribe(
+      result=>{
+        console.log(result);
+        if(result.status==401){
+          this.router.navigateByUrl('/login');
+        }else{
+          this.isValidUser = true;
+          this.drawHomePage(result);
+        }
+      }
+    )
+  }
+  selectChartType(chartName:any){
+    if(chartName=='line'){
+      this.chartName = 'Line Chart';
+    }
+    if(chartName=='pie'){
+      this.chartName = 'Pie Chart';
+    }
+      
+  }
+  drawHomePage(response:any){
+    for(let i=0;i<response.results.length;i++){
+      for(let j=0;j<response.results[i].length;j++){            
+            this.barChartLabels.push(response.results[i][0]);
+            this.barChartValues.push(response.results[i][1]);
+      }
+    }
   }
   downloadAsPDF(){
     var element = document.getElementById('pdfTable') as HTMLCanvasElement;
@@ -26,23 +75,31 @@ export class HomeComponent implements OnInit {
       }
     )
   }
+  downloadAsExcel(){
+    this.middlwareService.downloadAsExcel().subscribe(
+      (response)=>{      
+        let file = new Blob([response], { type: 'application/vnd.ms-excel' });
+        var fileURL = URL.createObjectURL(file);
+        console.log(fileURL);
+        window.open(fileURL, "_blank");
+
+      }
+    );
+  }
   public barChartOptions = {
-    scaleShowVerticalLines:false,
+    scaleShowVerticalLines:false,  
     responsive:true
   };
-  public barChartLabels = ['2006','2007','2008','2009','2010','2011','2012'];
-  public barChartType = 'bar';
-  public barChartLegend = true;
-
+  
   public barChartData = [
     {
-      data:[65,59,80,81,56,55,40],
+      data:this.barChartValues,//[65,59,80,81,56,55,40],
       label:'Series A'
-    },
+    }/* ,
     {
       data:[100,200,300,400,500],
       label:'Series B'
-    }
+    } */
   ]
  
 }
--- Angular/src/app/services/middlware.service.ts
+++ Angular/src/app/services/middlware.service.ts
@@ -9,6 +9,10 @@ export class MiddlwareService {
     private httpClient:HttpClient
   ) { }
   headers = new HttpHeaders().set('Content-Type', 'application/json; charset=utf-8');
+  httpOptions = {
+    //'responseType'  : 'arraybuffer' as 'json'
+    'responseType'  : 'blob' as 'json'        //This also worked
+  };
   //Registration related services...
   submitSignup(formData: any) {
     return this.httpClient.post<any>('Covid19Dashboard/signup', formData, {headers: this.headers});
@@ -22,5 +26,12 @@ export class MiddlwareService {
   //Login related services...
   login(formData:any){
     return this.httpClient.post<any>('Covid19Dashboard/login', formData, {headers: this.headers});
+  }
+  //Excel download Service
+  downloadAsExcel(){
+    return this.httpClient.get('Covid19Dashboard/downloadAsExcel',{responseType: 'blob'});
+  }
+  home(formData:any){
+    return this.httpClient.post<any>('Covid19Dashboard/home', formData, {headers: this.headers});
   }
 }
